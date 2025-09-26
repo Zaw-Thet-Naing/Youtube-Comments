e504a804c46f5b11423b35910dab92f4b520c680
amal <famal1295@gmail.com>
2025-09-26 18:30:18 +0800

    structure to hold information about each video

---
+++ Youtube_Comments.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <limits>
+
+using namespace std;
+
+struct Video {
+    string link;
+    string title;                                       // struct to define video information
+    string comments;
+};
