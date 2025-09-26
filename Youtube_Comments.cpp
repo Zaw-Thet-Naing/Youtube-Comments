495ee0df8019b7896d1da08641ff8347876f0313
amal <famal1295@gmail.com>
2025-09-26 18:30:56 +0800

    define a function to store video information

--- Youtube_Comments.cpp
+++ Youtube_Comments.cpp
@@ -12,3 +12,7 @@ struct Video {
     string title;                                       // struct to define video information
     string comments;
 };
+
+vector<Video> fetchVideos(const string& filename){
+    ifstream inputFile(filename);                       // vector store videos acquired from input txt file
+    vector<Video> videos;
